8dcd4da68b896011a13c37c1dd844f074dab551c
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-01-18 14:06:32 -0300

    Update StairClicker.js

--- StairClicker.js
+++ StairClicker.js
@@ -10,6 +10,6 @@ function stairUP() {
     playerData.lifetimePoints +=1
     playerData.caloriePoints += 0.17
     console.log(playerData.stairPoints, playerData.caloriePoints)
-    document.getElementById("pontua").innerHTML = playerData.stairPoints
+    document.getElementById("pontua").innerHTML = playerData.stairPoints.toFixed(0)
     document.getElementById("calorico").innerHTML = playerData.caloriePoints.toFixed(2)
 }
