2bbf6abd261c945b86c02963209a477f322ca584
Angelis <48072083+Angelion879@users.noreply.github.com>
2021-01-18 13:14:20 -0300

    Starting the project

---
+++ StairClicker.js
@@ -0,0 +1,15 @@
+let playerData = {
+    name: "",
+    stairPoints: 0,
+    caloriePoints: 0,
+    lifetimePoints: 0
+}
+
+function stairUP() {
+    playerData.stairPoints +=1
+    playerData.lifetimePoints +=1
+    playerData.caloriePoints += 0.17
+    console.log(playerData.stairPoints, playerData.caloriePoints)
+    document.getElementById("pontua").innerHTML = playerData.stairPoints
+    document.getElementById("calorico").innerHTML = playerData.caloriePoints.toFixed(2)
+}
